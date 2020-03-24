4610a285f1a58e02eddb0fef32dbf3bdf9dfda4c
Dai FUJIHARA@daipresents <daipresents@gmail.com>
2020-03-24 20:41:13 +0900

    add LCD and speaker sketch

--- LCD_and_speaker_for_ATP3011F4/LCD_and_speaker_for_ATP3011F4.ino
+++ LCD_and_speaker_for_ATP3011F4/LCD_and_speaker_for_ATP3011F4.ino
@@ -13,21 +13,30 @@
 SoftwareSerial serial(6, 7); //RX, TX
 LiquidCrystal lcd( 2, 4, 10, 11, 12, 13 );
 
-String MESSAGE = "konnitiwa watashiwa/fujihara/dai/desu.kyouwa yorosiku/onegai/simasu\r";
+String LINE1 = "konnitiwa watashiwa/fujihara/dai/desu.";
+String LINE2 = "kyouwa yorosiku/onegai/simasu\r";
 
 void setup() {
   serial.begin(9600);
-  
-  lcd.begin( 16, 2 );
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Hello! (^_^)");
-  lcd.setCursor(0, 1);
-  lcd.print("My name is Dai!");
 }
 
 void loop() {
+  lcd.begin( 16, 2 );
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  
+  // https://synapse.kyoto/lib/MGLCD/CharacterCode/page001.html
+  // コンニチハ ワタシハ
+  lcd.print("\xba\xdd\xc6\xc1\xca \xdc\xc0\xbc\xca");  
   serial.print("?");
-  serial.print(MESSAGE);
+  serial.print(LINE1);
+
   delay(1000);
-}
+  
+  // フジハラ ダイ デス 
+  lcd.setCursor(0, 1);
+  lcd.print("\xcc\xbc\xde\xca\xd7 \xc0\xde\xb2 \xc3\xde\xbd");
+  serial.print(LINE2);
+
+  delay(1000);
+  }
